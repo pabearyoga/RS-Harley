e2a39037f528224b99c65d3a47d000ae841c75f2
Pabearyoga <pabearyoga@gmail.com>
2023-07-28 00:05:14 +0300

    fix sendForm.js

--- js/sendForm.js
+++ js/sendForm.js
@@ -1,26 +1,63 @@
-// sendForm.js
+// Функція для відправки повідомлення на телеграм
+async function sendMessageToTelegram(messageText, chatId, botToken) {
+  try {
+    const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+    const data = {
+      chat_id: chatId,
+      text: messageText,
+      parse_mode: "HTML", // Якщо ви хочете використовувати HTML у повідомленні
+    };
 
-const form = document.getElementById("form1");
-console.log(form);
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  sendMessage(form);
-});
-
-async function sendMessage(form) {
-  const formData = new FormData(form);
-  if (formData) {
-    const url = "sendmessage.php";
     const response = await fetch(url, {
       method: "POST",
-      body: formData,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     });
-    if (response.ok) {
-      form.reset();
-      alert("Form sent!");
+
+    const responseData = await response.json();
+    if (responseData.ok) {
+      console.log("Повідомлення успішно відправлено!");
     } else {
-      alert("Error");
+      console.error("Помилка під час відправлення повідомлення:", responseData);
     }
+  } catch (error) {
+    console.error("Сталася помилка:", error);
   }
 }
+
+// Функція для обробки події надіслання форми
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
+  const messageText = "Дані з форми:\n\n" + formDataToString(formData);
+
+  // Замініть на ваш chat_id і токен бота
+  const chatId = "@RSmotoSendMessage";
+  const botToken = "6215398274:AAElV_0yRMADoBMXHY0c-b2BphpElUggaew";
+
+  sendMessageToTelegram(messageText, chatId, botToken);
+
+  form.reset();
+  alert("Form sent!");
+}
+
+// Функція для перетворення даних форми в рядок
+function formDataToString(formData) {
+  let result = "";
+  for (const entry of formData.entries()) {
+    const [key, value] = entry;
+    result += `<b>${key}</b>: ${value}\n`;
+  }
+  return result;
+}
+
+// Отримуємо всі форми на сторінці
+const forms = document.querySelectorAll(".form");
+
+// Додаємо обробник події надіслання форми до кожної форми
+forms.forEach((form) => {
+  form.addEventListener("submit", handleFormSubmit);
+});
