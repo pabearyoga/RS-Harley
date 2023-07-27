11c0afdac05e202feff3bb9d90eae1891739d2e4
Pabearyoga <pabearyoga@gmail.com>
2023-07-27 17:57:31 +0300

    add send form to telegram

---
+++ js/sendForm.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("form1");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  sendMessage(form);
+});
+
+async function sendMessage(form) {
+  const formData = new FormData(form);
+  if (formData) {
+    const url = "../php/sendmessage.php";
+    const response = await fetch(url, {
+      method: "POST",
+      body: formData,
+    });
+    if (response.ok) {
+      form.reset();
+      alert("Form sent!");
+    } else {
+      alert("Error");
+    }
+  }
+}
